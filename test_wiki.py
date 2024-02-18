38f5cb49fce7211080c3a31f050df77e220a5891
ichapman10 <ianchap@gmail.com>
2024-02-17 19:57:44 -0800

    Create test file

---
+++ test_wiki.py
@@ -0,0 +1,7 @@
+import unittest
+import WIkipedia_Data.py
+
+class TestExtractFunction(unittest.TestCase):
+    def test_extract_references(self):
+        html_content= []
+        self.assertTrue(data['parse']['text']['*'])
