770903dc54d4a68a84599c5f8593b4f88d6ebd1b
IChapman10 <157485785+IChapman10@users.noreply.github.com>
2024-02-20 21:09:57 -0800

    Added Search Bar

--- Wikipedia_Data.py
+++ Wikipedia_Data.py
@@ -56,7 +56,7 @@ def write_to_file(page_title, summary, references):
         for i, ref in enumerate(references, start=1):
             file.write(f"Reference {i}: {ref}\n")
 
-page_title = "Artificial Intelligence"
+page_title = input("Search Topic: ")
 html_content = get_wikipedia_page_content(page_title)
 
 if html_content:
